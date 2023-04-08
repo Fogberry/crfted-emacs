7a9b45e5cc0c1c27d74e2e8dc52e2e3c46808162
ShaMaTeTuanZhang <i@yinn.party>
2023-04-08 23:46:46 +0800

    upgrade lsp-mode and lsp-ui

--- crafted-emacs/lisp/init-lsp.el
+++ crafted-emacs/lisp/init-lsp.el
@@ -10,6 +10,14 @@
   (setq read-process-output-max (* 1024 1024)) ;; 1MB
   ;; set prefix for lsp-command-keymap (few alternatives - "C-l", "C-c l")
   (setq lsp-keymap-prefix "C-c l")
+  (setq lsp-signature-auto-activate nil
+        lsp-headerline-breadcrumb-enable nil
+        lsp-semantic-tokens-enable t
+        lsp-progress-spinner-type 'progress-bar-filled
+        lsp-enable-folding nil
+        lsp-enable-symbol-highlighting nil
+        lsp-enable-text-document-color nil
+        lsp-enable-on-type-formatting nil)
   :hook (;; replace XXX-mode with concrete major-mode(e. g. python-mode)
          (prog-mode . (lambda ()
                         (unless (derived-mode-p 'emacs-lisp-mode 'lisp-mode 'makefile-mode)
@@ -21,7 +29,45 @@
 ;; optionally
 (use-package lsp-ui
   :ensure t
-  :commands lsp-ui-mode)
+  :bind(("C-c u" . lsp-ui-imenu)
+        :map lsp-ui-mode-map
+        ("s-<return>" . lsp-ui-sideline-apply-code-actions))
+  :hook (lsp-mode . lsp-ui-mode)
+  :init
+  (setq lsp-ui-sideline-show-diagnostics nil
+        lsp-ui-sideline-ignore-duplicate t
+        lsp-ui-doc-delay 0.1
+        lsp-ui-doc-show-with-cursor (not (display-graphic-p))
+        lsp-ui-imenu-auto-refresh 'after-save
+        lsp-ui-imenu-colors `(,(face-foreground 'font-lock-keyword-face)
+                              ,(face-foreground 'font-lock-string-face)
+                              ,(face-foreground 'font-lock-constant-face)
+                              ,(face-foreground 'font-lock-variable-name-face)))
+  :config
+  (defun lsp-ui-peek--peek-display (src1 src2)
+    (-let* ((win-width (frame-width))
+            (lsp-ui-peek-list-width (/ (frame-width) 2))
+            (string (-some--> (-zip-fill "" src1 src2)
+                      (--map (lsp-ui-peek--adjust win-width it) it)
+                      (-map-indexed 'lsp-ui-peek--make-line it)
+                      (-concat it (lsp-ui-peek--make-footer))))
+            )
+      (setq lsp-ui-peek--buffer (get-buffer-create " *lsp-peek--buffer*"))
+      (posframe-show lsp-ui-peek--buffer
+                     :string (mapconcat 'identity string "")
+                     :min-width (frame-width)
+                     :poshandler #'posframe-poshandler-frame-center)))
+
+  (defun lsp-ui-peek--peek-destroy ()
+    (when (bufferp lsp-ui-peek--buffer)
+      (posframe-delete lsp-ui-peek--buffer))
+    (setq lsp-ui-peek--buffer nil
+          lsp-ui-peek--last-xref nil)
+    (set-window-start (get-buffer-window) lsp-ui-peek--win-start))
+
+  (advice-add #'lsp-ui-peek--peek-new :override #'lsp-ui-peek--peek-display)
+  (advice-add #'lsp-ui-peek--peek-hide :override #'lsp-ui-peek--peek-destroy)
+  )
 
 ;; optionally if you want to use debugger
 (use-package dap-mode
